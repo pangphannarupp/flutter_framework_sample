6632d9117b441c8d5704d92e590098a9846634ca
pangphannarupp <pangphannarupp@gmail.com>
2022-09-15 09:02:22 +0700

    first init

---
+++ android/app/src/main/kotlin/kh/phanna/flutter_framework_sample/flutter_framework_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package kh.phanna.flutter_framework_sample.flutter_framework_sample
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_framework/flutter_framework.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Biometric Authentication',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Biometric Authentication'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final plugin = SpeechRecognizerPlugin();
+          plugin.speechToText(language: 'km-KH', description: 'សូមនិយាយនៅទីនេះ',
+          onSuccess: (result) {
+            print(result);
+          });
+        },
+        child: Icon(Icons.face),
+      ),
+    );
+  }
+}
